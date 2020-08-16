209381a338853f29299774ab05928e3ea358941c
Abolfazl <Abolixes@gmail.com>
2020-08-16 15:25:04 +0430

    new file:   MySQL.py

---
+++ MySQL.py
@@ -0,0 +1,44 @@
+import mysql.connector
+import sys
+
+DB_NAME = "aura"
+DB_USER = "root"
+DB_PASS = "VC430ndwt8"
+DB_HOST = "localhost"
+
+
+
+def DBConnect(DatabaseHost,DatabaseName,DatabaseUser,DatabasePass):
+    try:
+        conn = mysql.connector.connect(
+        host=DatabaseHost,
+        database=DatabaseName,
+        user=DatabaseUser,
+        password=DatabasePass
+        )
+        conn.autocommit = True
+        # conn.autocommit(True)
+        cur = conn.cursor(dictionary=True)
+        print(f"Connected To {DB_NAME} .")
+        return conn , cur
+    except:
+        print('Error in Connecting to Database .')
+        sys.exit(1)
+
+conn, cur = DBConnect(DB_HOST,DB_NAME, DB_USER,DB_PASS)
+
+def GetMatch(Id):
+    SQLQuery = "SELECT * FROM matches WHERE id=%s"
+    SQLData = (Id,)
+    cur.execute(SQLQuery, SQLData)
+    Result = cur.fetchone()
+    if Result == None:
+        # It Means Match is not in database
+        return False
+    return Result
+
+
+def CreateMatch():
+    SQLQuery = "INSERT INTO matches (id, Team1Name, Team2Name, Team1Score, Team2Score, GoalData, Status) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+    SQLData = ()
+    cur.execute(SQLQuery,SQLData)
